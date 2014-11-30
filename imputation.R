bb608193e4edc89c93fbc8319a747b46cd75d3c0
Michael Nicholson <mjnichol@ualberta.ca>
2014-11-29 21:47:56 -0700

    fixed the multiple imputation!

--- imputation.R
+++ imputation.R
@@ -18,22 +18,51 @@ regress.imp <- function(missing.data.csv){
 
 # perform multiple imputation on the missing data csv file
 # This will output several frames!
-mult.imps <- function(missing.data.csv){
+mult.imps <- function(missing.data.csv, num_imps){
 	library("mice")
+	
+	if ( num_imps < 2) stop("Number of imputations is too low!")
 
 	missing.data <- read.table(missing.data.csv, header = TRUE, sep= ",", 
 		na.strings = c(" "))
 	# perform the default multiple imputation as defined by mice
-	imp <- mice(missing.data, printFlag = FALSE)
-	.impute.and.write(missing.data, imp$imp, missing.data.csv, "mult")
+	imp <- mice(missing.data, m = num_imps, printFlag = FALSE)
+	
+	# prepare the multiple imputation list for passing to the csv output
+	list.imp <- vector(mode="list", length = num_imps)	
+	for (i in 1:num_imps){
+		temp.imp = NULL		
+
+		for (j in 1:length(list.imp) ){
+			
+			temp.imp = c( temp.imp, imp$imp[[j]][i] )
+		}
+
+		list.imp[[i]] <- as.list(temp.imp)
+	}
+
+
+
+	for (i in 1:num_imps){
+					       
+		.impute.and.write(missing.data, list.imp[[i]], paste(i, missing.data.csv, sep="_"), "mult")
+	}
 }
 
 # reinsert and write the imputation results to a suitably named output file
 .impute.and.write <- function(missing.data, missing.vals, csv, type){
 
-	missing.data <- .reconstruct.missing.vals(missing.data, missing.vals)
+	missing.data <- .reconstruct.missing.vals(missing.data, missing.vals, type)
 	if( length( missing.data[is.na(missing.data)] ) > 0) stop("imputation failed: missing data still present")
 	write.csv(missing.data, file=paste(type, "imputation", csv, sep='_'))
+}
+
+# handle the case where you have to do multiple imputation, so you will be 
+# returning and writing a set of imputed values
+.multiple.imputation.handler <- function(imputation, missing.data){
+
+
+	return()
 }
 
 # take the result with missing data values and then take result of the 
@@ -41,7 +70,7 @@ mult.imps <- function(missing.data.csv){
 
 # Note that multiple imputation will take in and outout multiple frames
 # use a list to handle this possibility
-.reconstruct.missing.vals <- function(missing.frame, imputation){
+.reconstruct.missing.vals <- function(missing.frame, imputation, type){
 
 	complete.frame <- missing.frame 
 
