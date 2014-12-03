8b4be074c78618dc59f504508a24e484145756cc
Michael Nicholson <mjnichol@ualberta.ca>
2014-12-02 23:29:42 -0700

    Added run script

---
+++ run_script.R
@@ -0,0 +1,18 @@
+args<-commandArgs(TRUE)
+source("imputation.R")
+# first argument is the method
+# second argument is the csv
+# third argument is the number of times to run
+switch = toString(args[1])
+file = toString(args[2])
+iters = as.integer(args[3])
+
+if (switch == "mean.imp"){
+	mean.imp(file) 
+} else if (switch == "regress.imp"){
+	regress.imp(file)
+} else if (switch == "mult.imp"){
+	mult.imp(file, iters)
+} else {
+	stop("Imputation method does not exist!")
+}
